656f7f9ee2dfe5a0cd2f663774272e1d592204d0
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2021-06-05 22:16:36 -0300

    New cost function!

--- cost_function/src/main.cpp
+++ cost_function/src/main.cpp
@@ -15,51 +15,51 @@
 class CostFunction
 {
 public:
-	CostFunction() : _nh(""), _tfListener(_tfBuffer)
-	{
-		_costPub = _nh.advertise<std_msgs::Float64>("cost", 10);
-		_sourcePositionSub = _nh.subscribe("source_position", 1, &CostFunction::UpdateSourcePosition, this);
+    CostFunction() : _nh(""), _tfListener(_tfBuffer)
+    {
+        _costPub = _nh.advertise<std_msgs::Float64>("cost", 10);
+        _sourcePositionSub = _nh.subscribe("source_position", 1, &CostFunction::UpdateSourcePosition, this);
 
-		SetupParameters();
-	}
+        SetupParameters();
+    }
 
-	void UpdateSourcePosition(const geometry_msgs::Point::ConstPtr& point)
-	{
-		_lastSourcePosition.x = point->x;
-		_lastSourcePosition.y = point->y;
-	}
+    void UpdateSourcePosition(const geometry_msgs::Point::ConstPtr &point)
+    {
+        _lastSourcePosition.x = point->x;
+        _lastSourcePosition.y = point->y;
+    }
 
-	void Spin()
-	{
-		WaitTransformAvailable();
+    void Spin()
+    {
+        WaitTransformAvailable();
 
-		ros::Rate rate(GetParam<float>("~rate"));
-		while (_nh.ok())
-		{
-			auto transform = GetTransform();
+        ros::Rate rate(GetParam<float>("~rate"));
+        while (_nh.ok())
+        {
+            auto transform = GetTransform();
 
-			auto robot_x = transform.translation.x;
-			auto robot_y = transform.translation.y;
+            auto robot_x = transform.translation.x;
+            auto robot_y = transform.translation.y;
 
-			auto source_x = _lastSourcePosition.x;
-			auto source_y = _lastSourcePosition.y;
+            auto source_x = _lastSourcePosition.x;
+            auto source_y = _lastSourcePosition.y;
 
-			std_msgs::Float64 cost;
-			cost.data = Evaluate(robot_x, robot_y, source_x, source_y);
-			_costPub.publish(cost);
+            std_msgs::Float64 cost;
+            cost.data = Evaluate(robot_x, robot_y, source_x, source_y);
+            _costPub.publish(cost);
 
-			ros::spinOnce();
-			rate.sleep();
-		}
-	}
+            ros::spinOnce();
+            rate.sleep();
+        }
+    }
 
 private:
-	double Evaluate(double robot_x, double robot_y, double source_x, double source_y)
-	{
-		return -(pow(robot_x - source_x, 2) + pow(robot_y - source_y, 2)) + 400;
-	}	
+    double Evaluate(double robot_x, double robot_y, double source_x, double source_y)
+    {
+        return -(pow(robot_x - source_x, 2) + pow(robot_y - source_y, 2)) + 50;
+    }
 
-	geometry_msgs::Transform GetTransform()
+    geometry_msgs::Transform GetTransform()
     {
         try
         {
@@ -79,7 +79,7 @@ private:
         }
     }
 
-	void WaitTransformAvailable()
+    void WaitTransformAvailable()
     {
         while (!_tfBuffer.canTransform(child_frame, ref_frame, ros::Time(0), ros::Duration(1.0)))
         {
@@ -88,33 +88,36 @@ private:
         }
     }
 
-	void SetupParameters()
-	{
-		try {
-			ref_frame = GetParam<std::string>("~ref_frame");
-			child_frame = GetParam<std::string>("~robot_frame");
-		} catch (std::exception &e) {
-			ROS_FATAL("%s", e.what());
-			exit(-1);
-		}
-	}
+    void SetupParameters()
+    {
+        try
+        {
+            ref_frame = GetParam<std::string>("~ref_frame");
+            child_frame = GetParam<std::string>("~robot_frame");
+        }
+        catch (std::exception &e)
+        {
+            ROS_FATAL("%s", e.what());
+            exit(-1);
+        }
+    }
 
-	ros::NodeHandle _nh;
+    ros::NodeHandle _nh;
 
-	tf2_ros::Buffer _tfBuffer;
+    tf2_ros::Buffer _tfBuffer;
     tf2_ros::TransformListener _tfListener;
 
-	std::string ref_frame, child_frame;
+    std::string ref_frame, child_frame;
 
-	ros::Publisher _costPub;
+    ros::Publisher _costPub;
     ros::Subscriber _sourcePositionSub;
 
-	geometry_msgs::Point _lastSourcePosition;
+    geometry_msgs::Point _lastSourcePosition;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "cost_function");
+    ros::init(argc, argv, "cost_function");
 
     CostFunction node;
 
